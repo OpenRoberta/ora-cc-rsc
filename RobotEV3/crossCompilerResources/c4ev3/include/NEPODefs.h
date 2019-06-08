f685f5d50437d963bb9256ea0c3de994f12405ec
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-06-08 23:12:10 +0200

    OpenRoberta/openroberta-lab#29 split NEPODefs.h in multiple files and update c4ev3 headers and static library

--- RobotEV3/crossCompilerResources/c4ev3/include/NEPODefs.h
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPODefs.h
@@ -1,6 +1,9 @@
 #include <math.h>
 
-#define SPEED_MAX 100
+#define TEXT_COLOR_WHITE 0
+#define TEXT_COLOR_BLACK 1
+
+#define GOLDEN_RATIO 1.61803398875
 
 inline float MathMin(float firstValue, float secondValue) {
     return firstValue < secondValue ? firstValue : secondValue;
@@ -10,84 +13,6 @@ inline float MathMax(float firstValue, float secondValue) {
     return firstValue > secondValue ? firstValue : secondValue;
 }
 
-inline int Speed(float speed) {
-    return MathMax(-SPEED_MAX, MathMin(speed, SPEED_MAX));
-}
+#include "NEPOMotors.h"
+#include "NEPOSensors.h"
 
-/**
- * RotateMotor of c4ev3, called with a negative angle, will behave as if the angle was positive.
- * This function will run the motor in reverse if the angle is negative.
-*/
-inline void RotateMotorForAngleWithTurn(int outputs, float speed, float angle, float turn) {
-    if (angle < 0) {
-        speed *= -1;
-        angle *= -1;
-    }
-    RotateMotorEx(outputs, speed, angle, turn, true, true);
-}
-
-inline void RotateMotorForAngle(int outputs, float speed, float angle) {
-    RotateMotorForAngleWithTurn(outputs, speed, angle, 0);
-}
-
-
-inline void SteerDrive(int portLeft, int portRight, float speedLeft, float speedRight) {
-    OnFwdReg(portLeft, speedLeft);
-    OnFwdReg(portRight, speedRight);
-}
-
-inline void StopSteer (int portLeft, int portRight) {
-    Off(portLeft);
-    Off(portRight);
-}
-
-
-inline float computeOuterCircumferenceForSteerDrive (float speedLeft, float speedRight, float distance);
-inline float computeAngleForSteerDrive (float outerCircumference);
-inline void WaitSteerDriveToReachAngle(int portRight, int portLeft, float speedLeft, float speedRight, int angle);
-
-inline void SteerDriveForDistance(int portLeft, int portRight, float speedLeft, float speedRight, float distance) {
-    float outerCircumference = computeOuterCircumferenceForSteerDrive(speedLeft, speedRight, distance);
-    float angle = computeAngleForSteerDrive(outerCircumference);
-    SteerDrive(portLeft, portRight, speedLeft, speedRight);
-    WaitSteerDriveToReachAngle(portLeft, portRight, speedLeft, speedRight, angle);
-    StopSteer(portLeft, portRight);
-}
-
-inline float computeOuterCircumferenceForSteerDrive (float speedLeft, float speedRight, float distance) {
-    if (speedLeft + speedRight == 0) {
-        return distance;
-    } else if (abs(speedLeft) < abs(speedRight)) {
-        return speedLeft / (speedRight + speedLeft) * 2.0 * distance;
-    } else {
-        return speedRight / (speedRight + speedLeft) * 2.0 * distance;
-    }
-}
-
-inline float computeAngleForSteerDrive (float outerCircumference) {
-    return abs(outerCircumference * 360.0 / (M_PI * WHEEL_DIAMETER));
-}
-
-inline void WaitMotorToReachAngle(int motorToCheck, float speedToCheck, int angle);
-
-inline void WaitSteerDriveToReachAngle(int portLeft, int portRight, float speedLeft, float speedRight, int angle) {
-    if (abs(speedLeft) < abs(speedRight)) {
-        WaitMotorToReachAngle(portRight, speedRight, angle);
-    } else {
-        WaitMotorToReachAngle(portLeft, speedLeft, angle);
-    }
-}
-
-inline void WaitMotorToReachAngle(int motorPort, float speed, int angle) {
-    int start = MotorRotationCount(motorPort);
-    int targetEnd = start + (speed > 0 ? angle : -angle);
-    if (speed > 0) {
-        while(MotorRotationCount(motorPort) < targetEnd) {
-            Wait(1);
-        }
-    } else {
-        while(MotorRotationCount(motorPort) > targetEnd) {
-            Wait(1);
-        }
-    }
-}
---
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPOSensors.h
@@ -0,0 +1,15 @@
+#define DEFAULT_MODE_COLOR      COL_COLOR
+#define DEFAULT_MODE_GYRO       GYRO_ANG
+#define DEFAULT_MODE_INFRARED   IR_PROX
+#define DEFAULT_MODE_TOUCH      TOUCH_PRESS
+#define DEFAULT_MODE_ULTRASONIC US_DIST_CM
+#define DEFAULT_MODE_SOUND      NXT_SOUND_DB
+#define DEFAULT_MODE_COMPASS    NXT_COMPASS
+#define DEFAULT_MODE_IRSEEKER   IR_SEEK
+
+inline int ReadSensorInMode (int port, int mode) {
+    if (GetSensorName(port) != mode) {
+        SetSensorMode(port, mode);
+    }
+    return readSensor(port);
+}
