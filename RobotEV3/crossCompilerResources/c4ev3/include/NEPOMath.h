3ac9e35db0cf572761eae2880c3fb438419c4616
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-06-17 19:35:49 +0200

    OpenRoberta/openroberta-lab#28 add math functions

--- RobotEV3/crossCompilerResources/c4ev3/include/NEPOMath.h
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPOMath.h
@@ -3,7 +3,34 @@
 
 #include <math.h>
 
+// AbstractCppVisitor generates code that uses the PI constant instead of M_PI
+#define PI M_PI
 
 
+inline bool IsWhole(double d) {
+    return d == floor(d);
+}
+
+inline bool IsPrime(double d) {
+    if (!IsWhole(d)) {
+        return false;
+    }
+    int n = d;
+    if (n < 2) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
+    for (int i = 3, s = (int) (sqrt(d) + 1); i <= s; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 #endif
