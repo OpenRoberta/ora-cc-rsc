c27ee9def045716a1074b2a07ebe552b60a19415
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-06-17 17:00:53 +0200

    OpenRoberta/openroberta-lab#28 add beep when the program is uploaded to the EV3

---
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPOInit.h
@@ -0,0 +1,36 @@
+#ifndef NEPOINIT
+#define NEPOINIT
+
+#include <ev3.h>
+
+#define JUST_UPLOADED_FLAG_FILE_NAME "../prjs/BrkProg_SAVE/NEPO-just-uploaded.txt"
+
+bool NEPOProgramJustUploaded();
+void onNEPOProgramJustUploaded ();
+
+void inline NEPOInitEV3 () {
+    InitEV3();
+    if (NEPOProgramJustUploaded()) {
+        onNEPOProgramJustUploaded();
+    }
+}
+
+bool NEPOProgramJustUploaded () {
+     return access(JUST_UPLOADED_FLAG_FILE_NAME, F_OK) == 0;
+}
+
+
+void deleteJustUploadedFlag();
+
+void onNEPOProgramJustUploaded () {
+    PlaySound(SOUND_DOUBLE_BEEP);
+    Wait(1000);
+    deleteJustUploadedFlag();
+    exit(0);
+}
+
+void deleteJustUploadedFlag() {
+    remove(JUST_UPLOADED_FLAG_FILE_NAME);
+}
+
+#endif
