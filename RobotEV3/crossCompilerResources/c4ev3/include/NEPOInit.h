5e9e87be03ccd4cbb5d86067ee98075156d55c33
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-06-21 15:50:58 +0200

    OpenRoberta/openroberta-lab#29 stop the program when CENTER + DOWN buttons are pressed

--- RobotEV3/crossCompilerResources/c4ev3/include/NEPOInit.h
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPOInit.h
@@ -3,23 +3,30 @@
 
 #include <ev3.h>
 
+#define POOL_ESCAPE_SEQUENCE 100
+
 #define JUST_UPLOADED_FLAG_FILE_NAME "../prjs/BrkProg_SAVE/NEPO-just-uploaded.txt"
 
 bool NEPOProgramJustUploaded();
 void onNEPOProgramJustUploaded ();
+void startThreadToMonitorEscapeSequence();
 
 void inline NEPOInitEV3 () {
     InitEV3();
     if (NEPOProgramJustUploaded()) {
         onNEPOProgramJustUploaded();
     }
+    startThreadToMonitorEscapeSequence();
+}
+
+void inline NEPOFreeEV3 () {
+    FreeEV3();
+    exit(0);
 }
 
 bool NEPOProgramJustUploaded () {
-     return false;
-     //return access(JUST_UPLOADED_FLAG_FILE_NAME, F_OK) == 0;
+     return access(JUST_UPLOADED_FLAG_FILE_NAME, F_OK) == 0;
 }
-
 
 void deleteJustUploadedFlag();
 
@@ -27,12 +34,32 @@ void onNEPOProgramJustUploaded () {
     PlaySound(SOUND_DOUBLE_BEEP);
     Wait(1000);
     deleteJustUploadedFlag();
-    FreeEV3();
-    exit(0);
+    NEPOFreeEV3();
 }
 
 void deleteJustUploadedFlag() {
     remove(JUST_UPLOADED_FLAG_FILE_NAME);
+}
+
+
+void * escapeSequenceMonitor (void * arguments);
+
+void startThreadToMonitorEscapeSequence () {
+     pthread_t threadId;
+     pthread_create(&threadId, NULL, escapeSequenceMonitor, NULL);
+}
+
+bool isEscapeSequence ();
+
+void * escapeSequenceMonitor (void * arguments) {
+    while (!isEscapeSequence()) {
+        Wait(POOL_ESCAPE_SEQUENCE);
+    }
+    NEPOFreeEV3();
+}
+
+bool isEscapeSequence () {
+    return ButtonIsDown(BTNCENTER) && ButtonIsDown(BTNDOWN);
 }
 
 #endif
