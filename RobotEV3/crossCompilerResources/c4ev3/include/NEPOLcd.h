394f9022d001fef150fc0980f05abb76c3c6d93e
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-07-17 09:07:42 +0200

    OpenRoberta/openroberta-lab#29 fix RGB values (now from 0-255) and bug that printed RGB values using two LCD lines: change how values are converted to string and add a function to remove trailing zeros

--- RobotEV3/crossCompilerResources/c4ev3/include/NEPOLcd.h
+++ RobotEV3/crossCompilerResources/c4ev3/include/NEPOLcd.h
@@ -28,20 +28,4 @@ void DrawString (std::string str, int column, int row) {
     LcdPrintf(LCD_COLOR_BLACK, "%s", str.c_str());
 }
 
-template<typename T>
-void DrawString (std::list<T> strings, int column, int row) {
-    SetCursorColumn(column);
-    SetCursorRow(row);
-    int counter = 0;
-    for (auto str : strings) {
-        LcdPrintf(LCD_COLOR_BLACK, "%s ", ToString(str).c_str());
-        counter++;
-        if (counter < strings.size() - 1) {
-            LcdPrintf(LCD_COLOR_BLACK, ",");
-        }
-    }
-}
-
-
-
 #endif
